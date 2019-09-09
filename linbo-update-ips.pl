cac3c4bead25bb9fd700f086b1a34f37b1c011c5
Frank Schütte <fschuett@gymhim.de>
2019-09-09 08:23:07 +0200

    add_device: create missing workstations entry

--- linbo-update-ips.pl
+++ linbo-update-ips.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl -w
 # linbo-update-ips.pl
-#
-# Update ip, wlanip in workstations file
-#
+# if found: update ip, wlanip in workstations file
+# otherwise create line
+# workstation line: serverraum;cpqgymhim12;ipadmax;48:F1:7F:8F:F2:6E;10.0.22.68;;;;classroom-studentcomputer;;1;
+
 use strict;
+use JSON::XS;
 
 my $workstations = "/etc/linbo/workstations";
 my $temp = `mktemp /tmp/linbo-update-ipsXXXXXXXX`;
@@ -11,6 +13,7 @@ chomp $temp;
 my %host = ();
 my $IP = 4;
 my $WLANIP = 6;
+my $found = 0;
 
 while(<STDIN>){
     chomp;
@@ -30,8 +33,24 @@ while(<WORKSTATIONS>){
         $line[$IP] = $host{'ip'};
         $line[$WLANIP] = $host{'wlanIp'} if defined $host{'wlanIp'};
         $_ = join ';', @line;
+        $found = 1;
     }
     print TEMP "$_\n";
+}
+if( not $found and defined $host{'name'} and defined $host{'hwconf'} and defined $host{'mac'} and defined $host{'ip'} ){
+    my $result = `/usr/sbin/oss_api.sh get devices/byName/$host{'name'}`;
+    $result = eval { decode_json($result) };
+    if ($@){
+        print "decode_json failed, invalid json. error:$@\n";
+    } else {
+        $result = `/usr/sbin/oss_api.sh get rooms/$result->{'roomId'}`;
+        $result = eval { decode_json($result) };
+        if ($@){
+            print "decode_json failed, invalid json. error:$@\n";
+        } elsif( defined $result->{'name'} ){
+            print TEMP $result->{'name'}.";".$host{'name'}.";".$host{'hwconf'}.";".$host{'mac'}.";".$host{'ip'}.";".$host{'wlanMac'}.";".$host{'wlanIp'}.";;classroom-studentcomputer;;1;\n";
+        }
+    }
 }
 close(TEMP);
 close(WORKSTATIONS);
